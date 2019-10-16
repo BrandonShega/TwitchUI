ac869aa03827e8a0926e29cfae6ef3675a8f7e71
Brandon Shega <b.shega@gmail.com>
2019-10-15 21:41:41 -0400

    Initial Commit

---
+++ TwitchUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitchUI
+//
+//  Created by Shega, Brandon on 10/15/19.
+//  Copyright © 2019 Shega, Brandon. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
